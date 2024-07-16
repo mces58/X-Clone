8e1744af838e73508aad33fd7bf38a9a0afab7c6
mces58 <ceser446@gmail.com>
2024-07-16 15:23:00 +0300

    feat(user): added notification model and controller function

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,4 +1,7 @@
-const { default: User } = require('src/models/user.model');
+import bcrypt from 'bcryptjs';
+
+import Notification from 'src/models/notification.model';
+import User from 'src/models/user.model';
 
 const getUserProfile = async (req, res) => {
   const { userName } = req.params;
@@ -55,6 +58,14 @@ const followUnFollowUser = async (req, res) => {
     await me.updateOne({ $push: { following: id } });
     await user.updateOne({ $push: { followers: req.user._id } });
 
+    const newNotification = new Notification({
+      type: 'follow',
+      from: req.user._id,
+      to: id,
+    });
+
+    await newNotification.save();
+
     return res.status(200).json({
       success: true,
       message: 'User followed',
@@ -64,4 +75,87 @@ const followUnFollowUser = async (req, res) => {
   }
 };
 
-export { getUserProfile, followUnFollowUser };
+const getSuggestedUsers = async (req, res) => {
+  try {
+    const userId = req.user._id;
+
+    const usersFollowedByMe = await User.findById(userId).select('following');
+
+    const users = await User.aggregate([
+      {
+        $match: {
+          _id: { $ne: userId },
+        },
+      },
+      { $sample: { size: 10 } },
+    ]);
+
+    const filteredUser = users.filter(
+      (user) => !usersFollowedByMe.following.includes(user._id)
+    );
+    const suggestedUser = filteredUser.slice(0, 4);
+
+    suggestedUser.forEach((user) => {
+      user.password = null;
+    });
+
+    res.status(200).json({
+      success: true,
+      message: '',
+      data: suggestedUser,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const updateProfile = async (req, res) => {
+  const { fullName, userName, email, currentPassword, newPassword, bio, link } = req.body;
+  const { avatar, cover } = req.body;
+
+  const userId = req.user._id;
+
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: 'User not found', data: {} });
+    }
+
+    if ((!newPassword && currentPassword) || (!currentPassword && newPassword)) {
+      return res.status(400).json({
+        success: false,
+        message: 'Please provide both current password and new password',
+        data: {},
+      });
+    }
+
+    if (currentPassword && newPassword) {
+      const isMatch = await bcrypt.compare(currentPassword, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ success: false, message: 'Current password is incorrect', data: {} });
+      }
+      if (newPassword.length < 6) {
+        return res.status(400).json({
+          success: false,
+          message: 'Password must be at least 6 characters long',
+          data: {},
+        });
+      }
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(newPassword, salt);
+    }
+
+    // upload avatar to cloudinary
+    console.log(fullName, userName, email, bio, link, avatar, cover);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { getUserProfile, followUnFollowUser, getSuggestedUsers, updateProfile };
--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -1,16 +1,21 @@
 import { Router } from 'express';
 
-import { followUnFollowUser, getUserProfile } from 'src/controllers/user.controller';
+import {
+  followUnFollowUser,
+  getSuggestedUsers,
+  getUserProfile,
+  updateProfile,
+} from 'src/controllers/user.controller';
 import authentication from 'src/middlewares/authentication.middleware';
 
 const router = Router();
 
 router.route('/profile').get(authentication, getUserProfile);
 
-router.route('/suggested').get(authentication);
+router.route('/suggested').get(authentication, getSuggestedUsers);
 
 router.route('/follow/:id').post(authentication, followUnFollowUser);
 
-router.route('/update').put(authentication);
+router.route('/update').put(authentication, updateProfile);
 
 export default router;
